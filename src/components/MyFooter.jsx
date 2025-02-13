2be05f674473f9c94330d38fa722aab401a775dd
Giovanni Maria Ricciolini <151955659+GianmariaRicciolini@users.noreply.github.com>
2025-02-13 15:49:55 +0100

    end desktop layout

--- src/components/MyFooter.jsx
+++ src/components/MyFooter.jsx
@@ -2,8 +2,18 @@ import React from "react";
 
 function MyFooter() {
   return (
-    <div>
-      <h1>Footer</h1>
+    <div className="footer">
+      <div className="contact">
+        <h4>loremipsum.it</h4>
+        <p>E-mail: info@example.com</p>
+        <div>
+          <p>twitter</p>
+          <p>linkedin</p>
+        </div>
+      </div>
+      <div className="rights">
+        <p>© 2021 Tutti i diritti risevati - Design by MM</p>
+      </div>
     </div>
   );
 }
